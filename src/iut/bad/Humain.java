7d0f5659cdf0d73cf5e9c9f7fdccc82a8203b4d1
allanDani <allanrazafnirainy@gmail.com>
2024-04-03 08:36:09 -0700

    Details

--- src/iut/bad/Humain.java
+++ src/iut/bad/Humain.java
@@ -19,4 +19,34 @@ public class Humain {
 	        this.prenom = prenom;
 	        this.age = age;
 	    }
+	    
+	    public void details() {
+	        System.out.println("Nom: " + nom + ", Prénom: " + prenom + ", Âge: " + age);
+	    }
+
+		public String getNom() {
+			return nom;
+		}
+
+		public void setNom(String nom) {
+			this.nom = nom;
+		}
+
+		public String getPrenom() {
+			return prenom;
+		}
+
+		public void setPrenom(String prenom) {
+			this.prenom = prenom;
+		}
+
+		public int getAge() {
+			return age;
+		}
+
+		public void setAge(int age) {
+			this.age = age;
+		}
+	    
+	    
 }
