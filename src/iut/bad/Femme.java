060eb3acb3db73e4491f50ef5505e0d355acc660
allanDani <allanrazafnirainy@gmail.com>
2024-04-03 08:47:06 -0700

    Femme ami Homme

--- src/iut/bad/Femme.java
+++ src/iut/bad/Femme.java
@@ -4,5 +4,12 @@ public class Femme extends Humain{
 	 public Femme() {
 	        super();
 	    }
+	 
+	 public static void main(String[] args) {
+	        Femme femme = new Femme();
+	        Homme homme = new Homme();
+
+	        femme.ami(homme); // Déclare que la femme est amie avec l'homme
+	    }
 }
 
